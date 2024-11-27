af96ae1e31585789258262db2d5a036553c758dd
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-11-27 12:49:41 +0700

    component link untuk tautan

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,28 @@
+import Link from "next/link";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <header>header</header>
-        {children}
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/blog">Blog</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>{children}</main>
         <footer>footer</footer>
       </body>
     </html>
