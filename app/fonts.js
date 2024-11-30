2ccba7fb4e8a5a53e1e4e50fa7be2c3703a045a6
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-11-30 20:32:18 +0700

    font di next

---
+++ app/fonts.js
@@ -0,0 +1,11 @@
+import { Roboto, Poppins } from "next/font/google";
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
