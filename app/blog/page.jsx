63f2bc2b70d59fbbdb2fc47861b63b28e8e18669
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-01 19:14:15 +0700

    fungsi untuk list content

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,41 +1,17 @@
 import Heading from "@/components/Heading";
 import PostCard from "@/components/PostCard";
+import { getAllPosts } from "@/lib/post";
 import Link from "next/link";
 import React from "react";
 
-export default function BlogPage() {
+export default async function BlogPage() {
+  const listPost = await getAllPosts();
+  console.log(listPost);
+
   return (
     <>
       <Heading>Blog</Heading>
       <p>list of posts</p>
-      <ul>
-        <li>
-          <Link href="/blog/belajar-nextjs">Belajar Next.js</Link>
-        </li>
-        <li>
-          <Link href="/blog/latihan-route-nextjs">Belajar route next.js</Link>
-        </li>
-      </ul>
-      <PostCard
-        title={"Belajar Next.js"}
-        href={"/blog/belajar-nextjs"}
-        image={
-          "https://raw.githubusercontent.com/lunadiotic/learn-nextjs/refs/heads/master/public/images/image-1.jpg"
-        }
-        description={"Belajar Next.js"}
-        date={"2023-01-01"}
-        author={"Luna"}
-      />
-      <PostCard
-        title={"belajar Route Next.js"}
-        href={"/blog/latihan-route-nextjs"}
-        image={
-          "https://raw.githubusercontent.com/lunadiotic/learn-nextjs/refs/heads/master/public/images/image-2.jpg"
-        }
-        description={"Belajar Next.js"}
-        date={"2023-01-01"}
-        author={"Luna"}
-      />
     </>
   );
 }
