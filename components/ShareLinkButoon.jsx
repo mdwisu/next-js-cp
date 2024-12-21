494d7430c4f2c309c10e557067d7ec512e7c8213
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-21 17:39:36 +0700

    percantik halaman web dengan icon berbasis tailwindcss

--- components/ShareLinkButoon.jsx
+++ components/ShareLinkButoon.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { LinkIcon } from "@heroicons/react/20/solid";
 
 export default function ShareLinkButoon() {
   const [copied, setCopied] = useState(false);
@@ -13,8 +14,9 @@ export default function ShareLinkButoon() {
   return (
     <button
       onClick={handleCLick}
-      className="border px-2 py-1 rounded text-gray-500 text-sm hover:bg-gray-200 hover:text-gray-700"
+      className="border flex gap-1 items-center px-2 py-1 rounded text-gray-500 text-sm hover:bg-gray-200 hover:text-gray-700"
     >
+      <LinkIcon className="h-4 w-4" />
       {copied ? "Link Copied" : "Share Link"}
     </button>
   );
