e5fd832317c067436ae569aa48d697424bad606a
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-21 20:04:54 +0700

    menentukan data response dari strapi dengan parameter

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -1,6 +1,26 @@
 import { writeFileSync } from "fs";
+import qs from "qs";
 
-const url = "http://127.0.0.1:1337/api/posts" + "?populate=*";
+const url =
+  "http://127.0.0.1:1337/api/posts" +
+  "?" +
+  qs.stringify(
+    {
+      fields: ["slug", "title", "description", "publishedAt", "body"],
+      populate: {
+        image: {
+          fields: ["url"],
+        },
+      },
+      sort: ["publishedAt:desc"],
+      pagination: {
+        pageSize: 3,
+      },
+    },
+    {
+      encodeValuesOnly: true,
+    }
+  );
 const response = await fetch(url);
 const body = await response.json();
 const posts = JSON.stringify(body, null, 2);
