aaee067528b7d7e0e34089f05e76d819500117eb
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-22 14:21:22 +0700

    mendapatkan data dari query parameter untuk pagination

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,17 +1,24 @@
 import Heading from "@/components/Heading";
 import PostCard from "@/components/PostCard";
 import { getAllPosts } from "@/lib/post";
+import Link from "next/link";
 import React from "react";
 
 export const revalidate = 30;
 
-export default async function BlogPage() {
+export default async function BlogPage({ searchParams }) {
+  const page = parsePageParam(searchParams.page);
   const listPost = await getAllPosts();
 
   return (
     <>
       <Heading>Blog</Heading>
-      <p>list of posts</p>
+      <h2>list of posts</h2>
+      <div className="flex  gap-3 pb-3">
+        <Link href={`/blog?page=${page - 1}`}>&lt;</Link>
+        <span>Page {page}</span>
+        <Link href={`/blog?page=${page + 1}`}>&gt;</Link>
+      </div>
 
       {listPost.map((post) => {
         return (
@@ -28,4 +35,14 @@ export default async function BlogPage() {
       })}
     </>
   );
+}
+
+function parsePageParam(paramValue) {
+  if (paramValue) {
+    const page = parseInt(paramValue);
+    if (isFinite(page) && page > 0) {
+      return page;
+    }
+  }
+  return 1;
 }
